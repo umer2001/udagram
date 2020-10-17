1fe538e634264efcda0e7e2ff64e70dbae67934e
umer2001 <umer2001.uf@gmail.com>
2020-10-18 01:34:05 +0500

    test-10

---
+++ www/server.js
@@ -0,0 +1,104 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const path_1 = __importDefault(require("path"));
+const util_1 = require("./util/util");
+const fs_1 = __importDefault(require("fs"));
+(() => __awaiter(this, void 0, void 0, function* () {
+    // Init the Express application
+    const app = express_1.default();
+    // Set the network port
+    const port = process.env.PORT || 8082;
+    // Use the body parser middleware for post requests
+    app.use(body_parser_1.default.json());
+    // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+    // GET /filteredimage?image_url={{URL}}
+    // endpoint to filter an image from a public url.
+    // IT SHOULD
+    //    1
+    //    1. validate the image_url query
+    //    2. call filterImageFromURL(image_url) to filter the image
+    //    3. send the resulting file in the response
+    //    4. deletes any files on the server on finish of the response
+    // QUERY PARAMATERS
+    //    image_url: URL of a publicly accessible image
+    // RETURNS
+    //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+    /**************************************************************************** */
+    //! END @TODO1
+    // Root Endpoint
+    // Displays a simple message to the user
+    app.get("/", (req, res) => __awaiter(this, void 0, void 0, function* () {
+        res.send("try GET /filteredimage?image_url={{}}");
+    }));
+    // filter image --> render video --> delete tmp files endpoint
+    app.get("/filteredimage", (req, res) => __awaiter(this, void 0, void 0, function* () {
+        const url = req.query.image_url;
+        if (!url) {
+            res.status(400).send({ message: "url is null" });
+        }
+        try {
+            console.log("got it....");
+            const output = path_1.default.basename(url, ".jpg");
+            res.send(output);
+            const promises = [];
+            for (var i = 0; i <= 5; i++) {
+                promises.push(util_1.filterImageFromURL(url, i));
+            }
+            Promise.all(promises)
+                .then((results) => __awaiter(this, void 0, void 0, function* () {
+                console.log("All done", results);
+                yield util_1.renderVideo(output);
+                util_1.deleteLocalFiles(results);
+            }))
+                .catch((e) => {
+                // Handle errors here
+            });
+            // const filteredimage = await filterImageFromURL(url).then(renderVideo);
+            // console.log("filteredimage : " + filteredimage);
+            //res.status(200).send(filteredimage);
+        }
+        catch (error) {
+            res.status(422).send("invalid image url");
+        }
+    }));
+    //display all in json format
+    app.get("/display", (req, res) => __awaiter(this, void 0, void 0, function* () {
+        const dirPath = path_1.default.join(__dirname, "util/complete");
+        fs_1.default.readdir(dirPath, (err, files) => {
+            if (err) {
+                console.log("somthing went wrong -> " + err);
+                res.send("somthing went wrong -> " + err);
+            }
+            else {
+                console.log(files);
+                res.send(files);
+            }
+        });
+    }));
+    // send specific video
+    app.get("/display/:name", (req, res) => __awaiter(this, void 0, void 0, function* () {
+        const dirPath = path_1.default.join(__dirname, "util/complete");
+        res
+            .status(200)
+            .sendFile(`${dirPath}/${req.params.name}`, (e) => console.log(e));
+    }));
+    // Start the Server
+    app.listen(port, () => {
+        console.log(`server running http://localhost:${port}`);
+        console.log(`press CTRL+C to stop server`);
+    });
+}))();
+//# sourceMappingURL=server.js.map
---
+++ www/util/util.js
@@ -0,0 +1,77 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const Jimp = require("jimp");
+const util = require("util");
+var path = require("path");
+const exec = util.promisify(require("child_process").exec);
+const debug = false;
+const videoEncoder = "h264"; // mpeg4 libvpx
+var output;
+// filterImageFromURL
+// helper function to download, filter, and save the filtered images locally
+// returns the absolute path to the local image
+// INPUTS
+//    inputURL: string - a publicly accessible url to an image file
+// RETURNS
+//    an absolute path to a filtered image locally saved file
+function filterImageFromURL(inputURL, index) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise((resolve) => __awaiter(this, void 0, void 0, function* () {
+            try {
+                output = path.basename(inputURL, ".jpg");
+                const photo = yield Jimp.read(inputURL);
+                console.log(`/tmp/${output}.${index}.jpg`);
+                const outpath = `/tmp/${output}.${index}.jpg`;
+                yield photo
+                    .color([{ apply: "hue", params: [Number(index) * 10] }])
+                    .write(__dirname + outpath, (img) => {
+                    resolve(__dirname + outpath);
+                });
+            }
+            catch (error) {
+                console.log("errors ->" + error);
+            }
+        }));
+    });
+}
+exports.filterImageFromURL = filterImageFromURL;
+// renderVideo
+// helper function takes all images that are in tmp and make a video
+// uses a command line tool ffmpeg
+function renderVideo(name) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise((resolve) => __awaiter(this, void 0, void 0, function* () {
+            console.log("Encoding for " + name);
+            yield exec(`ffmpeg -start_number 1 -i src/util/tmp/${name}.%d.jpg -vcodec ${videoEncoder} -profile:v baseline -pix_fmt yuv420p -filter:v "setpts=20.5*PTS" src/util/complete/${output}.mp4`);
+            resolve(path.join(__dirname, "complete", `${output}.mp4`));
+        }));
+    });
+}
+exports.renderVideo = renderVideo;
+// deleteLocalFiles
+// helper function to delete files on the local disk
+// useful to cleanup after tasks
+// INPUTS
+//    files: Array<string> an array of absolute paths to files
+function deleteLocalFiles(files) {
+    return __awaiter(this, void 0, void 0, function* () {
+        for (let file of files) {
+            fs_1.default.unlinkSync(file);
+            console.log("deleting");
+        }
+    });
+}
+exports.deleteLocalFiles = deleteLocalFiles;
+//# sourceMappingURL=util.js.map
